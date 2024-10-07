d00e44646f33919dbf09ed5ced6ba1919226ba59
Esteban Barragan Muñoz <esteban@akaunt.io>
2024-10-07 14:45:09 -0700

    feat: install tailwind and migrate header

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,5 @@
-import { styled } from 'styled-components'
-const Button = styled.button`
-  & {
-    padding: 1rem 2rem;
-    font-weight: 600;
-    text-transform: uppercase;
-    border-radius: 0.25rem;
-    color: #1f2937;
-    background-color: #f0b322;
-    border-radius: 6px;
-    border: none;
-  }
-  &:hover {
-    background-color: #f0920e;
-  }
-`
-export default Button
+
+function Button({ children, ...props }) {
+  return <button {...props} className="py-2 px-4 uppercase font-semibold rounded text-stone-900 bg-amber-400 hover:bg-amber-500">{children}</button>
+}
+export default Button;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -52,11 +52,10 @@ const StyledHeader = styled.header`
 `
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      {/* <p className={classes.paragraph}>A community of artists and art-lovers.</p> */}
-      <p>A community of artists and art-lovers.</p>
-    </StyledHeader>
+    <header className='flex flex-col items-center mt-8 mb-8 md:mb-16'>
+      <img src={logo} alt="A canvas" className='object-contain mb-8 w-44 h-44' />
+      <h1 className='text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>ReactArt</h1>
+      <p className='text-stone-500'>A community of artists and art-lovers.</p>
+    </header>
   );
 }
